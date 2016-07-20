134d22ccf5fe60bb3659bbc06f8c0a2e894a7749
Vivien <vachet@enseirb.fr>
2016-07-20 17:21:40 +0200

    now using ctserver datas

--- www/js/app.js
+++ www/js/app.js
@@ -17,69 +17,32 @@ angular.module('starter', ['ionic', 'ngCordova', 'angucomplete-alt'])
 				$urlRouterProvider.otherwise('/home');  
 		})
 		.factory('choice', function($rootScope) {
-			 var authorization = {};
- 
+				var authorization = {};
+				
     		return {
         		getAuthObject: function () {
-        	    return authorization;
-        	},
-        	setAuthObject: function (authObject) {
-            	authorization = authObject;
-        	}
-        	};
+        				return authorization;
+        		},
+        		setAuthObject: function (authObject) {
+            		authorization = authObject;
+        		}
+        };
 		})
 		.factory('getDatasService', function($http) {
 				var _KEY = "8PVVD8CAA8";
 				return {
 						getLine: function(id) {
-								return $http.get("https://data.bordeaux-metropole.fr/wps?key="+_KEY+"&service=WPS&version=1.0.0&request=Execute&Identifier=saeiv_troncons_sens&DataInputs=GID%3D" + id,
-																 {transformResponse: function (cnv) {
-																		 var x2js = new X2JS();
-																		 var aftCnv = x2js.xml_str2json(cnv);
-																		 return aftCnv;
-																 }});
+								return $http.get("/data/lineGeometry?id=" + id);
 						},
 						getStops: function(lineId) {
-								return $http.get("https://data.bordeaux-metropole.fr/wps?key="+_KEY+"&service=WPS&version=1.0.0&request=Execute&Identifier=saeiv_arrets_sens&DataInputs=GID%3D"+lineId,
-																 {transformResponse: function (cnv) {
-																		 var x2js = new X2JS();
-																		 var aftCnv = x2js.xml_str2json(cnv);
-																		 return aftCnv;
-																 }});
+								return $http.get("/data/stationsPosition?id=" + lineId);
 						},
-						getTrams: function(lineId, sens) {
-								var filter = "%3CFilter%3E%3CAND%3E%3CPropertyIsEqualTo%3E%3CPropertyName%3ERS_SV_LIGNE_A%3C%2FPropertyName%3E%3CLiteral%3E"+lineId+"%3C%2FLiteral%3E%3C%2FPropertyIsEqualTo%3E%3CPropertyIsEqualTo%3E%3CPropertyName%3ESENS%3C%2FPropertyName%3E%3CLiteral%3E"+sens+"%3C%2FLiteral%3E%3C%2FPropertyIsEqualTo%3E%3C%2FAND%3E%3C%2FFilter%3E";
-								return $http.get("https://data.bordeaux-metropole.fr/wfs?key="+_KEY+"&REQUEST=GetFeature&SERVICE=WFS&TYPENAME=SV_VEHIC_P&VERSION=1.1.0&Filter="+filter,
-																 {transformResponse: function (cnv) {
-																		 var x2js = new X2JS();
-																		 var aftCnv = x2js.xml_str2json(cnv);
-																		 return aftCnv;
-																 }});
+						getVehicle: function(lineId, sens) {
+								return $http.get("/data/getVehicle?id=" + lineId + "&sens=" + sens);								
 						}
 				}
 		})
 		.factory('drawInformations', function($http) {
-				function rgf93tomercator(point) {
-						var firstProjection = '+proj=lcc +lat_1=44.25 +lat_2=45.75 +lat_0=45 +lon_0=3 +x_0=1700000 +y_0=4200000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs ';
-						var secondProjection = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs';
-						var res = proj4(firstProjection,secondProjection, point);
-						return {lat: res[1], lng: res[0]}
-				}
-
-				function getLatLngTrams(data) {
-						var pos = data.SV_VEHIC_P.msGeometry.Point.pos.__text.split(" ").map(Number);
-						var latlng = rgf93tomercator(pos);
-						return latlng;
-				}
-
-				function getLatLngStops(data) {
-						if(typeof data.Data.ComplexData.featureMember.SV_ARRET_P.Geometry.Point.pos.__text != 'undefined') {
-								var pos = data.Data.ComplexData.featureMember.SV_ARRET_P.Geometry.Point.pos.__text.split(" ").map(Number);
-								var latlng = rgf93tomercator(pos);
-						}
-						return latlng;
-				}
-
 				function getLabel(data) {
 						var lateTime = data.RETARD.__text;
 						var _text = "<span class='infospan' style='color:red;'> En retard de " + lateTime + "s</span>";
@@ -120,11 +83,16 @@ angular.module('starter', ['ionic', 'ngCordova', 'angucomplete-alt'])
 						drawMarkersTrams: function(datas, style, map) {
 								var copyMarkerTrams = this.markerTrams;
 								this.markerTrams = [];
-								
 								for(var i = 0; i < datas.length; ++i) {
-										var positionLatLng = getLatLngTrams(datas[i]);
-										var contentString = getTramInformation(datas[i].SV_VEHIC_P);
-										
+										console.log(datas[i]);
+										var positionLatLng = {lat: datas[i].lat, lng: datas[i].lng};
+//										var contentString = getTramInformation(datas[i]);
+										var contentString = '<h1 style="font-size: 20px; margin: 0;">Direction '+ datas[i].next +'</h1>'+
+												'Prochain arret: ' + datas[i].next + "<br>" +
+												'Retard: ' + datas[i].timing + "<br>" +
+												'Vitesse: ' + datas[i].speed + "<br>" +
+												'</div>';
+
 										var marker = new google.maps.Marker({
 												position: positionLatLng,
 												map: map,
@@ -138,7 +106,6 @@ angular.module('starter', ['ionic', 'ngCordova', 'angucomplete-alt'])
 										google.maps.event.addListener(marker, 'click', function() {  
 												var marker_map = this.getMap();
 												this.info.open(marker_map, this);	// If you call open() without passing a marker (this), the InfoWindow position will suck.
-
 										});
 										
 										this.markerTrams.push(marker);
@@ -148,13 +115,12 @@ angular.module('starter', ['ionic', 'ngCordova', 'angucomplete-alt'])
 						},
 						drawMarkersStops: function(datas, style, map) {
 								for(var i = 0; i < datas.length; ++i) {
-										var positionLatLng = getLatLngStops(datas[i]);
 										var contentString = '<div class="info-div-stop">'+
-												'<h1 style="font-size: 20px; margin: 0;">Station '+ datas[i].Data.ComplexData.featureMember.SV_ARRET_P.LIBELLE.__text +'</h1>'+
+												'<h1 style="font-size: 20px; margin: 0;">Station '+ datas[i].name +'</h1>'+
 												'</div>';
 
 										var marker = new google.maps.Marker({
-												position: positionLatLng,
+												position: {lat: parseFloat(datas[i].lat), lng: parseFloat(datas[i].lng)} ,
 												map: map,
 												icon: {url: style, anchor: new google.maps.Point(7,7), scaledSize: new google.maps.Size(14,14)}
 										});
@@ -171,60 +137,23 @@ angular.module('starter', ['ionic', 'ngCordova', 'angucomplete-alt'])
 
 								}
 						},
-						drawLines: function(lineId, map) {
-								function drawLine(polyline, datas) {
-										var listPaths = datas.split(" ").map(Number);
-										var path = [];
-										for(var j = 0; j+1 < listPaths.length; j+=2) {
-												var mercatorpos = rgf93tomercator([listPaths[j], listPaths[j+1]]);
- 												if(mercatorpos.lat > 43 && mercatorpos.lat < 45 && mercatorpos.lng > -1 && mercatorpos.lng < 0) {
-														path.push(mercatorpos);
-												}
-										}
-										polyline.setPath(path);
-								}
-
+						drawLines: function(datas, map) {
 								var options = {
-										strokeColor: '#F2A00E',
+										strokeColor: 'violet',
 										strokeOpacity: 1.0,
 										strokeWeight: 3,
 										map: map
 								};
-								
-								var poly_b = new google.maps.Polyline(options);
-								var poly_b_bis = new google.maps.Polyline(options);
-								var poly_a = new google.maps.Polyline(options);
-								var poly_a_bis = new google.maps.Polyline(options);
-								var poly_c = new google.maps.Polyline(options);
 
-								if(lineId == 59) { //ligne A
-										$http.get("datas/datas_a.txt").then(
-												function(answer) {
-														drawLine(poly_a, answer.data);
-												});
-										$http.get("datas/datas_a_bis.txt").then(
-												function(answer) {
-														drawLine(poly_a_bis, answer.data);
-												});
+								for(var i = 0; i < datas.length; i++) {
+										var current_stop = datas[i];
+										var current_line = new google.maps.Polyline(options);
+										var path = [];
+										for(var j = 0; j < current_stop.length; j++) {
+												path.push(current_stop[j]);
+										}
+										current_line.setPath(path);
 								}
-								
-								if(lineId == 60) { //ligne B
-										$http.get("datas/datas_b.txt").then(
-												function(answer) {
-														drawLine(poly_b, answer.data);
-												});
-										$http.get("datas/datas_b_bis.txt").then(
-												function(answer) {
-														drawLine(poly_b_bis, answer.data);
-												});
-								}								
-								
-								if(lineId == 61) { //ligne C
-										$http.get("datas/datas_c.txt").then(
-												function(answer) {
-														drawLine(poly_c, answer.data);
-												});
-								}								
 						}
 				};
 		})
@@ -304,19 +233,23 @@ angular.module('starter', ['ionic', 'ngCordova', 'angucomplete-alt'])
 				
 				getDatasService.getStops(_TRAM).then(
 						function(answer) {
-								var ans = answer.data.ExecuteResponse.ProcessOutputs.Output;
+								var ans = answer.data.results;
 								drawInformations.drawMarkersStops(ans, icon_stop, map);
 						});
-				
-				drawInformations.drawLines(_TRAM, map);
-				
-				getDatasService.getTrams(_TRAM, _SENS).then(
+
+				getDatasService.getLine(_TRAM).then(
 						function(answer) {
-								var ans = answer.data.FeatureCollection.featureMember;
+								drawInformations.drawLines(answer.data.results, map);
+						});
+				
+				getDatasService.getVehicle(_TRAM, _SENS).then(
+						function(answer) {
+								var ans = answer.data.results.vehicules;
 								drawInformations.drawMarkersTrams(ans, icon_tram, map);								
 						});
+
 				$interval(function() { 
-						getDatasService.getTrams(_TRAM, _SENS).then(
+						getDatasService.getVehicle(_TRAM, _SENS).then(
 						function(answer) {
 								var ans = answer.data.FeatureCollection.featureMember;
 								drawInformations.drawMarkersTrams(ans, icon_tram, map);								
